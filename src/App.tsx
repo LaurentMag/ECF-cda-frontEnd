2264a55f9349669dff3fb291b481fbd2de6468af
Laurent <magrez0laurent@gmail.com>
2022-10-13 09:56:52 +0200

    feature: router installation in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {Fragment} from "react";
 //
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+//
+import {HomePage} from "./pages/HomePage";
+import {Clients} from "./pages/Clients";
+import {Vehicules} from "./pages/Vehicules";
 
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/vehicules"
+            element={<Vehicules />}
+          />
+          <Route
+            path="/clients"
+            element={<Clients />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
+  );
 }
 
 export default App;
