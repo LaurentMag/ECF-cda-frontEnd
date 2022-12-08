6a1d24721a2070352cfc33c7a00421d2af9afa1d
Laurent <magrez0laurent@gmail.com>
2022-12-08 15:44:58 +0100

    WIP: to ignore

--- src/type/clientType.ts
+++ src/type/clientType.ts
@@ -3,6 +3,8 @@ export type clientType = {
   nom: string;
   prenom: string;
   dateDeNaissance: string;
+  // use local date to iso
+  // date format => et donne le format que tu veux pour la deserialisation du JSON
   email: string;
   telephone: string;
 };
@@ -14,3 +16,42 @@ export type clientTypeNoID = {
   email: string;
   telephone: string;
 };
+
+export class ClientModel {
+  private id: number = 0;
+  private nom: string;
+  private prenom: string;
+  private dateDeNaissance: string;
+  private email: string;
+  private telephone: string;
+
+  constructor(nom: string, prenom: string, dateDeNaissance: string, email: string, telephone: string) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.dateDeNaissance = dateDeNaissance;
+    this.email = email;
+    this.telephone = telephone;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  setId(id: number) {
+    this.id = id;
+  }
+}
+
+export class ClientNoIdModel {
+  private prenom: string;
+  private dateDeNaissance: string;
+  private email: string;
+  private telephone: string;
+
+  constructor(prenom: string, dateDeNaissance: string, email: string, telephone: string) {
+    this.prenom = prenom;
+    this.dateDeNaissance = dateDeNaissance;
+    this.email = email;
+    this.telephone = telephone;
+  }
+}
